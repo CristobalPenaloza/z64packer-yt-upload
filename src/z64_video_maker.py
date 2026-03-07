6eb7474e53c8d9ff8ebcd8df0e111a4faa04a2cb
CristobalPenaloza <41912913+CristobalPenaloza@users.noreply.github.com>
2026-03-06 22:25:48 -0300

    UTF-8 fixes for z64_video_maker.py

--- src/z64_video_maker.py
+++ src/z64_video_maker.py
@@ -312,7 +312,7 @@ def register_video():
 
                     # Write changes to database
                     databaseFile.seek(0)
-                    json.dump(database, databaseFile, indent=2)
+                    json.dump(database, databaseFile, indent=2, ensure_ascii=False)
                     databaseFile.truncate()
 
     print("Video registered successfully!")
